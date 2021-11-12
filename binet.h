9f15d5fe5d1fd9df25701f69bcf9dc7473ecab3e
henrisama <pedropiresflores@icloud.com>
2021-11-12 15:32:22 -0300

    binet fibonacci

--- binet.h
+++ binet.h
@@ -1,6 +1,4 @@
 #include <cmath>
-
 unsigned long long int binet(int n){
     return (1/sqrt(5)) * (pow((1 + sqrt(5))/2, n) - (pow((1 - sqrt(5))/2, n)));
-}
-// Corresponding to the nth Fibonacci number
+}// Corresponding to the nth Fibonacci number
